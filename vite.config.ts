eb1a134382e328f44486fbc89ccab59fa608689c
María Fernanda Torres Garrifo <mafe@MacBook-Air-de-Maria.local>
2025-10-01 22:23:34 -0500

    Configure for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: process.env.NODE_ENV === 'production'
+    ? '/trip-budget-maker/' // Replace with your repo name
+    : '/'
 }));
